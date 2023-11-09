c3d4f083c55dd1749e262b6d800a5c54bfb5f1db
Luis Fernández Couñago <lfcounago@gradiant.org>
2023-11-09 22:06:55 +0100

    fix: Update create group activity
    · Update the activity of create a new group adding the members when the user introduce the email of each member
    
    Refs: GH-15

--- app/src/main/java/com/lfcounago/gastoscompartidos/LoginActivity.java
+++ app/src/main/java/com/lfcounago/gastoscompartidos/LoginActivity.java
@@ -80,10 +80,8 @@ public class LoginActivity extends AppCompatActivity {
                             Toast.makeText(LoginActivity.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                             progressBar.setVisibility(View.GONE);
                         }
-
                     }
                 });
-
             }
         });
 
