85c52eadfe7f3008c54785197fc661e9b65015ae
Luis Fernández Couñago <lfcounago@gradiant.org>
2023-11-13 21:38:05 +0100

    fix: Layout and activity to list user groups
    · Create the layout to list user groups
    · Create the activity implementing the logic to list user groups
    · Create the base of the layout and the activity implementing for GroupDetailsActivity that will be implementing in Escenario 3
    
    Refs: GH-18

---
+++ app/src/main/java/com/lfcounago/gastoscompartidos/GroupDetailsActivity.java
@@ -0,0 +1,67 @@
+package com.lfcounago.gastoscompartidos;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class GroupDetailsActivity extends AppCompatActivity {
+
+    // Declarar los atributos de la clase
+    private TextView textViewName; // El componente que muestra el nombre del grupo
+    private TextView textViewCurrency; // El componente que muestra la divisa del grupo
+    private TextView textViewCategory; // El componente que muestra la categoría del grupo
+    private TextView textViewDescription; // El componente que muestra la descripción del grupo
+    private String groupId; // El id del grupo
+    private FirebaseFirestore db; // La referencia a la base de datos de Firestore
+
+    // Sobreescribir el método onCreate que se ejecuta al crear la actividad
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_details); // Establecer el layout correspondiente
+
+        // Inicializar los atributos de la clase
+        textViewName = findViewById(R.id.textViewName); // Obtener la referencia al componente textViewName del layout
+        textViewCurrency = findViewById(R.id.textViewCurrency); // Obtener la referencia al componente textViewCurrency del layout
+        textViewCategory = findViewById(R.id.textViewCategory); // Obtener la referencia al componente textViewCategory del layout
+        textViewDescription = findViewById(R.id.textViewDescription); // Obtener la referencia al componente textViewDescription del layout
+        groupId = getIntent().getStringExtra("groupId"); // Obtener el id del grupo como un extra del intent que inició la actividad
+        db = FirebaseFirestore.getInstance(); // Obtener la instancia de la base de datos de Firestore
+
+        // Llamar al método que obtiene los datos del grupo
+        getGroupData();
+    }
+
+    // Definir el método que obtiene los datos del grupo
+    private void getGroupData() {
+        // Realizar una consulta al documento del grupo en la colección "groups" de la base de datos de Firestore
+        db.collection("groups").document(groupId)
+                .get() // Obtener el documento
+                .addOnCompleteListener(task -> { // Añadir un listener que se ejecuta cuando la tarea se completa
+                    if (task.isSuccessful()) { // Si la tarea se ha completado con éxito
+                        // Obtener el resultado de la tarea como un documento
+                        DocumentSnapshot document = task.getResult();
+                        if (document != null) { // Si el documento no es nulo
+                            if (document.exists()) { // Si el documento existe
+                                // Obtener el nombre del grupo como el valor del campo "name" del documento
+                                String name = document.getString("name");
+                                // Obtener la divisa del grupo como el valor del campo "currency" del documento
+                                String currency = document.getString("currency");
+                                // Obtener la categoría del grupo como el valor del campo "category" del documento
+                                String category = document.getString("category");
+                                // Obtener la descripción del grupo como el valor del campo "description" del documento
+                                String description = document.getString("description");
+                                // Establecer el texto de los componentes con los datos obtenidos
+                                textViewName.setText(name);
+                                textViewCurrency.setText(currency);
+                                textViewCategory.setText(category);
+                                textViewDescription.setText(description);
+                            }
+                        }
+                    }
+                });
+    }
+}
---
+++ app/src/main/java/com/lfcounago/gastoscompartidos/ListUserGroupsActivity.java
@@ -0,0 +1,101 @@
+package com.lfcounago.gastoscompartidos;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListUserGroupsActivity extends AppCompatActivity {
+
+    // Declarar los atributos de la clase
+    private ListView listView; // El componente que muestra la lista de grupos
+    private ArrayAdapter<String> adapter; // El adaptador que vincula los datos con la vista
+    private List<String> groupNames; // La lista de nombres de los grupos
+    private List<String> groupIds; // La lista de ids de los grupos
+    private String uid; // El id del usuario actual
+    private FirebaseFirestore db; // La referencia a la base de datos de Firestore
+
+    // Sobreescribir el método onCreate que se ejecuta al crear la actividad
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_user_groups); // Establecer el layout correspondiente
+
+        // Inicializar los atributos de la clase
+        listView = findViewById(R.id.listView); // Obtener la referencia al componente listView del layout
+        groupNames = new ArrayList<>(); // Crear una lista vacía para los nombres de los grupos
+        groupIds = new ArrayList<>(); // Crear una lista vacía para los ids de los grupos
+        uid = FirebaseAuth.getInstance().getCurrentUser().getUid(); // Obtener el id del usuario actual
+        db = FirebaseFirestore.getInstance(); // Obtener la instancia de la base de datos de Firestore
+
+        // Crear un adaptador que vincula los nombres de los grupos con la vista del listView
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, groupNames);
+        listView.setAdapter(adapter); // Establecer el adaptador al listView
+
+        // Añadir un listener al listView que se activa cuando se hace clic en un elemento de la lista
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Obtener el id del grupo correspondiente al elemento pulsado
+                String groupId = groupIds.get(position);
+                // Crear un intent para iniciar una nueva actividad que muestre los datos del grupo
+                Intent intent = new Intent(ListUserGroupsActivity.this, GroupDetailsActivity.class);
+                // Añadir el id del grupo como un extra al intent
+                intent.putExtra("groupId", groupId);
+                // Iniciar la nueva actividad
+                startActivity(intent);
+            }
+        });
+
+        // Llamar al método que obtiene los grupos a los que pertenece el usuario
+        getGroups();
+    }
+
+    // Definir el método que obtiene los grupos a los que pertenece el usuario
+    private void getGroups() {
+        // Realizar una consulta a la colección "groups" de la base de datos de Firestore
+        db.collection("groups")
+                .get() // Obtener todos los documentos de la colección
+                .addOnCompleteListener(task -> { // Añadir un listener que se ejecuta cuando la tarea se completa
+                    if (task.isSuccessful()) { // Si la tarea se ha completado con éxito
+                        // Obtener el resultado de la tarea como una lista de documentos
+                        QuerySnapshot result = task.getResult();
+                        if (result != null) { // Si el resultado no es nulo
+                            // Recorrer cada documento del resultado
+                            for (QueryDocumentSnapshot document : result) {
+                                // Obtener el id del documento como el id del grupo
+                                String groupId = document.getId();
+                                // Obtener el nombre del grupo como el valor del campo "name" del documento
+                                String groupName = document.getString("name");
+                                // Obtener la divisa del grupo como el valor del campo "currency" del documento
+                                String groupCurrency = document.getString("currency");
+                                // Obtener la categoría del grupo como el valor del campo "category" del documento
+                                String groupCategory = document.getString("category");
+                                // Obtener la lista de usuarios del grupo como el valor del campo "users" del documento
+                                List<String> groupUsers = (List<String>) document.get("users");
+                                if (groupUsers != null) { // Si la lista de usuarios no es nula
+                                    if (groupUsers.contains(uid)) { // Si la lista de usuarios contiene el id del usuario actual
+                                        // Añadir el id del grupo a la lista de ids de los grupos
+                                        groupIds.add(groupId);
+                                        // Añadir el nombre del grupo, la divisa y la categoría a la lista de nombres de los grupos
+                                        groupNames.add(groupName + "\n" + groupCurrency + " - " + groupCategory);
+                                    }
+                                }
+                            }
+                            // Notificar al adaptador que los datos han cambiado
+                            adapter.notifyDataSetChanged();
+                        }
+                    }
+                });
+    }
+}
