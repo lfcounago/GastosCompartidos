b938f352bc8d4f12553f6df91a8f3a988c8d7e7a
silviaglezz <silvia.gulans@gmail.com>
2023-11-30 11:45:01 +0100

    Rebase onto main
    Refs: GH-41

--- app/src/main/java/com/lfcounago/gastoscompartidos/LoginActivity.java
+++ app/src/main/java/com/lfcounago/gastoscompartidos/LoginActivity.java
@@ -76,7 +76,7 @@ public class LoginActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
                             Toast.makeText(LoginActivity.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(), ListUserGroupsActivity.class));
+                            startActivity(new Intent(getApplicationContext(), BalanceActivity.class));
                         }else {
                             Toast.makeText(LoginActivity.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                             progressBar.setVisibility(View.GONE);
