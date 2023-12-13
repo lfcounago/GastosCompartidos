852188282ca38ea1bde6d06e506104c666208087
silviaglezz <silvia.gulans@gmail.com>
2023-12-13 14:58:34 +0100

    Menu lateral
    Creacion de menu lateral sin funcionalidades
    
    Refs: GH-45

--- app/src/main/java/com/lfcounago/gastoscompartidos/ListUserGroupsActivity.java
+++ app/src/main/java/com/lfcounago/gastoscompartidos/ListUserGroupsActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.Menu;
 import android.view.View;
 import android.view.Window;
 import android.widget.AdapterView;
