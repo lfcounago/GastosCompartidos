36d232820196c8e493dfe5e7fcbb6766a459ae24
NoeAnDi <noeliaandrade06@outlook.com>
2023-12-16 00:06:55 +0100

    fix: Pagina de liquidacion de deudas
    Casi todo completo
    
    Refs: GH-32

---
+++ app/src/main/java/com/lfcounago/gastoscompartidos/DebtLiquidationActivity.java
@@ -0,0 +1,164 @@
+package com.lfcounago.gastoscompartidos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DebtLiquidationActivity extends AppCompatActivity {
+
+    // Declarar los atributos de la clase
+    private ListView lvUsuarios;
+    private List<String> usuarios, uids, idsUsuario;
+    private Map<String, String> uidToName, nameToUid;
+    private ArrayAdapter<String> adapter;
+    private String gid, titulo;
+    private String uidU;
+    private Double gastoUsuario;
+    private FirebaseFirestore fStore;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_debt_liquidation);
+
+        // Inicializar los atributos de la clase
+        lvUsuarios = (ListView) findViewById(R.id.listView);
+        usuarios = new ArrayList<>();
+        uids = new ArrayList<>();
+        idsUsuario = new ArrayList<>();
+        uidToName = new HashMap<>();
+        nameToUid = new HashMap<>();
+        titulo = "Liquidación";
+        gid = "bmMYudVqJVuPzEaWwVNj";//getIntent().getStringExtra("groupId");//"bmMYudVqJVuPzEaWwVNj";
+        uidU = "uivt6Bi7ZjapLuBKUXF8e052Oku2";//FirebaseAuth.getInstance().getCurrentUser().getUid();//"uivt6Bi7ZjapLuBKUXF8e052Oku2";
+        fStore = FirebaseFirestore.getInstance();
+
+        // Crear un adaptador que vincula los nombres de los usuarios con la vista del listView
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, usuarios);
+        lvUsuarios.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String userId = idsUsuario.get(position);
+                String groupId = gid;
+                Double spendUsu = gastoUsuario;
+                Intent intent = new Intent(DebtLiquidationActivity.this, SettleDebtActivity.class);
+                intent.putExtra("IDUser", userId);
+                intent.putExtra("groupId", groupId);
+                intent.putExtra("spendUsu", spendUsu);
+                startActivity(intent);
+            }
+        });
+
+        // Llamar al método que obtiene los gastos
+        getSpends(gid);
+    }
+
+    //Recorre la colección "spends" para obtener todos los datos necesarios
+    private void getSpends(String groupId) {
+        List<Number> gastos = new ArrayList<>(); //Lista con los gastos del grupo
+
+        // Realizar una consulta a la colección "spends" de la base de datos de Firestore
+        fStore.collection("spends")
+                .get() // Obtener todos los documentos de la colección
+                .addOnCompleteListener(task -> { // Añadir un listener que se ejecuta cuando la tarea se completa
+                    if (task.isSuccessful()) {
+                        // Obtener el resultado de la tarea como una lista de documentos
+                        QuerySnapshot result = task.getResult();
+                        if (result != null) {
+                            // Recorrer cada documento del resultado
+                            for (QueryDocumentSnapshot document : result) {
+                                // Obtener los valores correspondientes
+                                String title = document.getString("title");
+                                Number amount = document.getDouble("amount");
+                                String groupID = document.getString("groupID");
+                                // Limpiar la lista de nombres de los usuarios
+                                usuarios.clear();
+                                // Limpiar los mapas de correspondencia entre UID y nombre de los usuarios
+                                uidToName.clear();
+                                nameToUid.clear();
+                                if (groupID != null && groupID.equals(groupId) && amount != null) { // Si la lista de gastos contiene el id del grupo actual
+                                    if(title != titulo) {// Añadir el gasto del grupo a la lista de gastos del grupo
+                                        gastos.add(amount);
+                                        //Cogemos los usuarios correspondientes al gasto
+                                        uids = (List<String>) document.get("sharedWith");
+                                    }
+                                }
+                            }
+                            totalExpenses(gastos, uids); //Llama a la función que obtiene los gastos del grupo y del "payer"
+                        }
+                    }
+                });
+    }
+
+    private void totalExpenses(List<Number> gastos, List<String> uidsUsu) {
+        double gastosTotales = 0;
+        int numUsu = uidsUsu.size();
+
+        for (Number amount : gastos) { //Calcula el gasto total del grupo
+            gastosTotales += amount.doubleValue();
+        }
+
+        double gastoUsu = round2(gastosTotales/numUsu);
+        gastoUsuario = gastoUsu;
+        for(String uid : uidsUsu){
+            if(!uid.equals(uidU)) {
+                // Obtener el documento correspondiente al UID del usuario de la colección users de Firebase
+                fStore.collection("users").document(uid)
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    // Obtener el documento del usuario
+                                    DocumentSnapshot document = task.getResult();
+                                    if (document.exists()) {
+                                        // Obtener el nombre del usuario
+                                        String name = document.getString("fName");
+                                        // Añadir el nombre del usuario a la lista de nombres de los usuarios
+                                        usuarios.add(name + "   -   " + gastoUsu);
+                                        // Añadir la correspondencia entre el UID y el nombre del usuario a los mapas
+                                        uidToName.put(uid, name);
+                                        nameToUid.put(name, uid);
+                                        idsUsuario.add(uid);
+                                        //Actualizar el adaptador
+                                        lvUsuarios.setAdapter(adapter);
+                                        adapter.notifyDataSetChanged();
+                                    } else {
+                                        Toast.makeText(DebtLiquidationActivity.this, "El documento del usuario no existe", Toast.LENGTH_SHORT).show();
+                                    }
+                                } else {
+                                    Toast.makeText(DebtLiquidationActivity.this, "Error al obtener el usuario", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        }
+    }
+
+    //Método para hacer el redonde a 2 cifras decimales
+    public static double round2(double value) {
+
+        return Math.round(value * 100.0) / 100.0;
+    }
+
+}
---
+++ app/src/main/java/com/lfcounago/gastoscompartidos/SettleDebtActivity.java
@@ -0,0 +1,193 @@
+package com.lfcounago.gastoscompartidos;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+public class SettleDebtActivity extends AppCompatActivity {
+
+    // Declaración de variables para los elementos de la interfaz
+    private TextView tvTitulo, tvCantidad, tvPagador, tvUsuDeuda;
+    private Button btGuardar;
+
+    // Declaración de variables para almacenar los datos
+    private String usuConectado, titulo, grupo, groupId, userId;
+    private double amount;
+
+    // Declaración de variables para acceder a la base de datos de Firebase
+    private FirebaseFirestore fStore;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settle_debt);
+
+        // Inicialización de las variables para los elementos de la interfaz
+        tvTitulo = findViewById(R.id.tvTitulo);
+        tvCantidad = findViewById(R.id.tvCantidad);
+        tvPagador = findViewById(R.id.tvPagador);
+        tvUsuDeuda = findViewById(R.id.tvUsuDeuda);
+        btGuardar = findViewById(R.id.btGuardar);
+
+        fStore = FirebaseFirestore.getInstance();
+
+        //Obtener el groupId que se pasa con el intent
+        groupId = getIntent().getStringExtra("groupId");
+        userId = getIntent().getStringExtra("IDUser");
+        amount = getIntent().getDoubleExtra("spendUsu", 0.0);
+        usuConectado = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        if (groupId == null && userId == null && usuConectado == null) {
+            Toast.makeText(this, "Datos nulos", Toast.LENGTH_SHORT).show();
+        }
+
+        // Obtener la fecha actual
+        Calendar calendar = Calendar.getInstance();
+        // Crear un formato de fecha
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+        // Obtener la fecha en formato de cadena
+        String fechaActual = dateFormat.format(calendar.getTime());
+
+        //Obtener referencia al documento del usuario conectado que paga la deuda por el ID
+        fStore.collection("users").document(usuConectado)
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        DocumentSnapshot document = task.getResult();
+                        if (document.exists()) {
+                            // Obtener el nombre del primer usuario
+                            String nameUsuPag = document.getString("fName");
+                            tvPagador.setText(nameUsuPag);
+                        }
+                    }
+                });
+        //Obtener referencia al documento del usuario conectado que se le paga la deuda por su ID
+        DocumentReference userRef2 = fStore.collection("users").document(userId);
+
+        userRef2.get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        DocumentSnapshot document = task.getResult();
+                        if (document.exists()) {
+                            // Obtener el nombre del primer usuario
+                            String nameUsu = document.getString("fName");
+                            tvUsuDeuda.setText(nameUsu);
+                        }
+                    }
+                });
+        actualizarInterfazUsuario();
+
+        //Obtener el nombre del grupo mediante su ID
+        fStore.collection("groups").document(groupId)
+                .get()
+                        .addOnCompleteListener(task -> {
+                            if(task.isSuccessful()){
+                                DocumentSnapshot document = task.getResult();
+                                if(document.exists()){
+                                    String nameGroup = document.getString("name");
+                                    grupo = nameGroup;
+                                }
+                            }
+                        });
+
+        // Listener para el botón de guardar
+        btGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                titulo = "Liquidación";
+                // Comprobar si los datos son válidos
+                if (validarDatos()) {
+                    // Crear un mapa para almacenar los datos del gasto
+                    Map<String, Object> gasto = new HashMap<>();
+                    gasto.put("title", titulo);
+                    gasto.put("date", fechaActual);
+                    gasto.put("amount", amount);
+                    gasto.put("payer", usuConectado);
+                    gasto.put("sharedWith", userId);
+                    gasto.put("groupID", groupId);
+                    gasto.put("group", grupo);
+                    // Añadir el documento del gasto a la colección spends de Firebase
+                    fStore.collection("spends")
+                            .add(gasto)
+                            .addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                                @Override
+                                public void onComplete(@NonNull Task<DocumentReference> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(SettleDebtActivity.this, "Liquidación guardada con éxito", Toast.LENGTH_SHORT).show();
+                                        toDebtLiquidation();
+                                    } else {
+                                        Toast.makeText(SettleDebtActivity.this, "Error al guardar el gasto", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                } else {
+                    Toast.makeText(SettleDebtActivity.this, "Los datos no son válidos", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    // Método para validar los datos introducidos por el usuario
+    private boolean validarDatos() {
+        if (titulo.isEmpty()) {
+            return false;
+        }
+        if (amount <= 0) {
+            return false;
+        }
+        return true;
+    }
+
+    // Método para cuando se elimina grupo volver a la pantalla principal de la aplicación ListUserGroupsActivity
+    private void toDebtLiquidation() {
+        // Crear un Intent para iniciar la actividad de detalles del grupo
+        Intent intent = new Intent(this, DebtLiquidationActivity.class);
+        startActivity(intent);
+        // Cerrar la actividad actual si es necesario
+        finish();
+    }
+
+    public void goToActividadAnterior(View view) {
+        // Crear un intent para iniciar la actividad DebtLiquidationActivity
+        Intent intent = new Intent(this, DebtLiquidationActivity.class);
+
+        startActivity(intent);
+    }
+
+    private void actualizarInterfazUsuario() {
+        tvTitulo.setText("Liquidación");
+        tvCantidad.setText(String.valueOf(amount));
+    }
+
+}
